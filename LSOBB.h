cc9b29b451f653167856342c43c746b57c03620a
Nicolás Vilurón <84143742+NiviDev@users.noreply.github.com>
2021-09-08 13:44:35 -0300

    Se cambio el alta

--- LSOBB.h
+++ LSOBB.h
@@ -9,7 +9,7 @@ int perteneceLSOBB(lista l, int codigo);
 
 
 void altaLSOBB(lista *l, Articulo articulo1, int *exito){
-    printf("Ejecuta la alta");
+    printf("Ejecuta la alta de lista SOBB 2.0");
 }
 
 void bajaLSOBB(lista *l, char codigo1[10], int *exito){
