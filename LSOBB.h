c20e3d53eca6206b2c2f814293db058ffce8e48e
Nicolás Vilurón <84143742+NiviDev@users.noreply.github.com>
2021-09-19 18:15:27 -0300

    Update LSOBB.h

--- LSOBB.h
+++ LSOBB.h
@@ -2,30 +2,118 @@
 #define LSOBB_H_INCLUDED
 
 ///Prototipo
-void altaLSOBB(lista *l, Articulo articulo1, int *exito);
-void bajaLSOBB(lista *l, char codigo1[10], int *exito);
-Articulo evocarLSOBB(lista l,int codigo, int *exito);
-int perteneceLSOBB(lista l, int codigo);
+int localizarLSOBB(lista *l, char codigo[10], int *exito);
+void altaLSOBB(lista *l, Articulo articulo, int *exito);
+void bajaLSOBB(lista *l, char codigo[10], int *exito);
+Articulo evocarLSOBB(lista *l,char codigo[10], int *exito);
+int perteneceLSOBB(lista *l, char codigo[10]);
 
-//Comentario
-void altaLSOBB(lista *l, Articulo articulo1, int *exito){
-    printf("Ejecuta la alta de lista SOBB 2.0, WEN");
-}
-// PROBANDO
-// probando 2.0
+///Funciones
+int localizarLSOBB(lista *l, char codigo[10], int *exito){ //inclusivo, exclusivo, derecha, derecha
+    int li = 0, ls = l->ult+1, t = 0, i;
+    *exito = 0;
+    if(ls!=0){
+        while(li != (ls-1)){
+            t=floor((li+ls)/2.0);
+            if(strcasecmp(l->listaArticulo[t].codigo,codigo) <= 0){
+                li=t;
+            }else{
+                ls=t;
+            }
 
-void bajaLSOBB(lista *l, char codigo1[10], int *exito){
-    printf("Ejecuta la baja");
+        }
+        t=ls-1;
+        if(strcasecmp(l->listaArticulo[t].codigo,codigo) == 0){
+            *exito = 1;
+        }else{
+            if(t==-1){
+                t++;
+            }else{
+                if(strcasecmp(l->listaArticulo[t].codigo,codigo) < 0){
+                    t++;
+                }
+            }
+
+
+        }
+    }
+    return t;
 }
 
-Articulo evocarLSOBB(lista l,int codigo, int *exito){
-    Articulo nulo;
-    printf("Ejecuta el evocar\n");
-    return nulo;
+
+
+void altaLSOBB(lista *l, Articulo articulo, int *exito){
+    int pos, i;
+    pos = localizarLSOBB(l,articulo.codigo,exito);
+    if(*exito == 0){
+        for(i=l->ult+1; i>=pos+1; i--){
+            strcpy(l->listaArticulo[i].codigo,l->listaArticulo[i-1].codigo);
+            strcpy(l->listaArticulo[i].tipo,l->listaArticulo[i-1].tipo);
+            strcpy(l->listaArticulo[i].marca,l->listaArticulo[i-1].marca);
+            strcpy(l->listaArticulo[i].descripcion,l->listaArticulo[i-1].descripcion);
+            l->listaArticulo[i].precio = l->listaArticulo[i-1].precio;
+            l->listaArticulo[i].cantidad = l->listaArticulo[i-1].cantidad;
+        }
+        l->ult++;
+        strcpy(l->listaArticulo[i].codigo, articulo.codigo);
+        strcpy(l->listaArticulo[i].tipo, articulo.tipo);
+        strcpy(l->listaArticulo[i].marca, articulo.marca);
+        strcpy(l->listaArticulo[i].descripcion, articulo.descripcion);
+        l->listaArticulo[i].precio = articulo.precio;
+        l->listaArticulo[i].cantidad = articulo.cantidad;
+        *exito = 1;
+    }else{
+        *exito = 0;
+    }
 }
 
-int perteneceLSOBB(lista l, int codigo){
-    printf("Pertenece\n");
-    return 0;
+
+void bajaLSOBB(lista *l, char codigo[10], int *exito){
+    int pos, aux=0,i;
+    pos=localizarLSOBB(l, codigo, exito);
+    if(*exito == 1){
+        mostrarArticulo(l->listaArticulo[pos]);
+        printf("\n\t\tDesea eliminar el articulo?\n\t\t1 - Eliminar\n\t\t2 - Volver\n\n\t\tOpcion: ");
+        fflush(stdin);
+        scanf("%d",&aux);
+        //fflush(stdin);
+        //getchar();
+        while(aux!=1&&aux!=2){
+            system("cls");
+            printf("\n\t\tOpcion invalido, ingrese nuevamente.\n\t\t1 - Eliminar\n\t\t2 - Volver\n\t\tOpcion: ");
+            fflush(stdin);
+            scanf("%d",&aux);
+            //fflush(stdin);
+            //getchar();
+        }
+        if(aux==1){ //decide eliminar
+            for(i=pos; i<l->ult;i++){
+                strcpy(l->listaArticulo[i].codigo,l->listaArticulo[i+1].codigo);
+                strcpy(l->listaArticulo[i].tipo,l->listaArticulo[i+1].tipo);
+                strcpy(l->listaArticulo[i].marca,l->listaArticulo[i+1].marca);
+                strcpy(l->listaArticulo[i].descripcion,l->listaArticulo[i+1].descripcion);
+                l->listaArticulo[i].precio = l->listaArticulo[i+1].precio;
+                l->listaArticulo[i].cantidad = l->listaArticulo[i+1].cantidad;
+            }
+            l->ult--;
+        }else{
+            *exito = 2; // no decide eliminar
+        }
+    }
 }
+
+Articulo evocarLSOBB(lista *l,char codigo[10], int *exito){
+    int pos;
+    pos = localizarLSOBB(l, codigo, exito);
+    if(*exito == 1){
+        return l->listaArticulo[pos];
+    }
+}
+
+int perteneceLSOBB(lista *l, char codigo[10]){
+    int exito;
+    localizarLSOBB(l,codigo,&exito);
+    return exito;
+}
+
 #endif // LSOBB_H_INCLUDED
